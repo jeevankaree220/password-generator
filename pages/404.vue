62b96785fedf3cbb434233f1f959e096b088d52e
venky-79 <venkateshwari.s94@gmail.com>
2024-04-07 18:08:04 +0530

    updated text

--- pages/404.vue
+++ pages/404.vue
@@ -5,7 +5,7 @@
           <div>
             <p class="mb-2 text-xs font-semibold tracking-wide text-gray-500 uppercase">Error 404</p>
             <h1 class="mb-4 text-2xl font-extrabold leading-tight tracking-tight text-left text-gray-900 md:text-4xl">Oops! The page you're looking for isn't here.</h1>
-            <p class="mb-5 text-base text-left text-gray-800 md:text-xl">You might have the wrong address, or the page may have moved.</p>
+            <p class="mb-5 text-base text-left text-gray-800 md:text-xl">You might have the wrong address, or the page may have moved!</p>
             <a href="/" class="w-full mb-2 bg-teal-500 text-white border-white hover:bg-teal-700 hover:text-white hover:border-white btn btn-lg btn-light sm:w-auto sm:mb-0">Back to homepage</a>
           </div>
           <div>
